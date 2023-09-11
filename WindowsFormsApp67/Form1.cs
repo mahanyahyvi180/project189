17131bd1cacfb12e70067e0f74c1b3c46a798c3c
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-11 10:04:30 +0330

    Add files via upload

---
+++ WindowsFormsApp67/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp67
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] x = new int[] { 10, 20, 30, 40 };
+            for (int i = 0; i < x.GetUpperBound(0); i++)
+                listBox1.Items.Add(x[i]);
+        }
+    }
+}
